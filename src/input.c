74c7b090ff56ec8fcb00f7f2e9abf49198988d9d
adaramm <ariel.mercado@ingenieria.unam.edu>
2025-12-01 11:59:34 -0600

    Add input_read function for reading input
    Initial implementation of input_read function.

---
+++ src/input.c
@@ -0,0 +1,10 @@
+#include "input.h"
+#include <stdio.h>
+
+// TODO: cambiar por getch() en Windows
+// Similar a la función que lee la entrada para el juego de snake
+char input_read() {
+    char c;
+    
+    return c;
+}
