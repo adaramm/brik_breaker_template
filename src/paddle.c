9075e4ba94bb9219ed1d821e79de0b4048cb0468
adaramm <ariel.mercado@ingenieria.unam.edu>
2025-12-01 12:15:17 -0600

    Implement paddle initialization and update functions
    Implemented basic paddle functions and added direction handling.

--- src/paddle.c
+++ src/paddle.c
@@ -1,7 +1,15 @@
 #include "paddle.h"
-#include <stdio.h>
 
-// TODO: implementar barra
-void paddle_init() {}
-void paddle_update() {}
-void paddle_render() {}
+static Paddle paddle;
+
+void paddle_init() {
+    // TODO: colocar el paddle centrado
+}
+
+void paddle_update(int direction) {
+    // TODO: mover el paddle según la dirección (-1 izquierda, 1 derecha)
+}
+
+Paddle* paddle_get() {
+    return &paddle;
+}
